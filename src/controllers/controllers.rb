248015a7542abc49c088a0a881fbd1f69395c4b5
Carlos Rodriguez <ucroxlive@gmail.com>
2021-12-12 21:48:57 -0500

    Terminacion de la implementacion del movimiento de la serpiente

--- src/controllers/controllers.rb
+++ src/controllers/controllers.rb
@@ -36,5 +36,23 @@ module Controllers
     end
   end
 
-  def position_is_valid?(state, next_position); end
+  def position_is_valid?(state, position)
+    # Verificar que ste dentro de la grilla
+    is_invalid = ((position.row >= state.grid.rows || position.row < 0) || (position.col >= state.grid.cols || position.col < 0))
+    return false if is_invalid
+
+    # Verificar que no se este superponiendo a la serpiente
+    !(state.snake.positions.include? position)
+  end
+
+  def move_snake_to_next_position(state, next_position)
+    new_positions = [next_position] + state.snake.positions[1...-1]
+    state.snake.position = new_positions
+    state
+  end
+
+  def end_game(state)
+    state.game_finished = true
+    state
+  end
 end
