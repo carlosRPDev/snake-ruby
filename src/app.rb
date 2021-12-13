0932b1739aa9b57db2abb3b139f9239dde7a1362
Carlos Rodriguez <ucroxlive@gmail.com>
2021-12-13 01:05:30 -0500

    Se implementa el timer del movimiento de la serpiente, se cambia el nombre del metodo render a render_game por colision de metodos

--- src/app.rb
+++ src/app.rb
@@ -2,19 +2,28 @@
 
 require_relative 'view/ruby2d'
 require_relative 'model/state'
+require_relative 'controllers/controllers'
 
 # Clase para ejecutar los diferentes acciones del juego
 class App
-  def start
-    view = View::Ruby2dView.new
-    initial_state = Model.init_state
-    view.render(initial_state)
+  def initialize
+    @estado = Model.init_state
   end
 
-  def init_timer
+  def start
+    view = View::Ruby2dView.new
+    Thread.new { init_timer(view) }
+    view.start(@estado)
+  end
+
+  def init_timer(view)
     loop do
+      @estado = Controllers.move_snake(@estado)
+      view.render_game(@estado)
       sleep 0.5
-      # Tigger movement
     end
   end
 end
+
+app = App.new
+app.start
